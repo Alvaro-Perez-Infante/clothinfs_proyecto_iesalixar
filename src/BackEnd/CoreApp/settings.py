9d7de4b55471d558c399f9718f06bf8e8f487449
Alvaro-Perez-Infante <alvaroperezin@gmail.com>
2024-06-11 16:23:10 +0200

    Commit Fix Settings y BD

--- src/BackEnd/CoreApp/settings.py
+++ src/BackEnd/CoreApp/settings.py
@@ -1,22 +1,18 @@
-import os
 from pathlib import Path
+import os
 import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Obtén la clave secreta del entorno, si no está definida, usa una clave por defecto (no recomendable en producción)
-SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
-
-# Modo de depuración activado si no estamos en el entorno de Render
+SECRET_KEY = os.environ.get('SECRET_KEY',default='your secret key')
 DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ["*"]
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
-INSTALLED_APPS = [
-    # Aplicaciones base de Django y terceros
+RENDER_EXTERNAL_HOSTNAME=os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME: ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+# Application definition
+BASE_APPS = [
     'admin_interface',
     'colorfield',
     'django.contrib.admin',
@@ -25,31 +21,45 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',  # Comunica puertos BackEnd + FrontEnd
-    'coreapi',
-    'rest_framework',
-    'import_export',
-    'django_ckeditor_5',
-    # Tus aplicaciones locales
+]
+
+LOCAL_APPS = [
     'ClothApp',
     'Auth',
 ]
 
+
+THIRDS_APPS = [
+    'corsheaders', # Comunica puertos BackEnd + FrontEnd
+    'coreapi',
+    'rest_framework',
+    'import_export',
+    'django_ckeditor_5',
+   
+]
+
+INSTALLED_APPS = BASE_APPS+THIRDS_APPS+LOCAL_APPS
+
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Middleware para servir archivos estáticos
+    # Whitenoise
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # Fin Whitenoise
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',  # Middleware para CORS
+    # Cors
+    'corsheaders.middleware.CorsMiddleware',
+    # Fin-Cors
 ]
 
 ROOT_URLCONF = 'CoreApp.urls'
 
-# Configuración de plantillas
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -68,15 +78,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'CoreApp.wsgi.application'
 
-# Configuración de la base de datos
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
+
 }
 
-# Validadores de contraseña
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -93,34 +103,133 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = 'es-es'
+
 TIME_ZONE = 'UTC'
+
 USE_I18N = True
+
 USE_TZ = True
 
-STATIC_URL = '/static/'
+
+STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-else:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
+    STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+    STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWS_CREDENTIALS = True
 
-REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
-}
+REST_FRAMEWORK = {'DEFAULT_SCHEMA_CLASS':'rest_framework.schemas.coreapi.AutoSchema',}
 
-X_FRAME_OPTIONS = "SAMEORIGIN"
+X_FRAME_OPTIONS = "SAMEORIGIN" #Para el custom admin
 
-MEDIA_URL = '/media/'
+#-------------------CKEDITOR-------------------
+
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+customColorPalette = [
+    {
+        'color': 'hsl(4, 90%, 58%)',
+        'label': 'Red'
+    },
+    {
+        'color': 'hsl(340, 82%, 52%)',
+        'label': 'Pink'
+    },
+    {
+        'color': 'hsl(291, 64%, 42%)',
+        'label': 'Purple'
+    },
+    {
+        'color': 'hsl(262, 52%, 47%)',
+        'label': 'Deep Purple'
+    },
+    {
+        'color': 'hsl(231, 48%, 48%)',
+        'label': 'Indigo'
+    },
+    {
+        'color': 'hsl(207, 90%, 54%)',
+        'label': 'Blue'
+    },
+]
+
+CKEDITOR_5_CONFIGS = {
+'default': {
+    'toolbar': ['heading', '|', 'bold', 'italic', 'link',
+                'bulletedList', 'numberedList', 'blockQuote', 'imageUpload', ],
+
+},
+'extends': {
+    'blockToolbar': [
+        'paragraph', 'heading1', 'heading2', 'heading3',
+        '|',
+        'bulletedList', 'numberedList',
+        '|',
+        'blockQuote',
+    ],
+    'toolbar': ['heading', '|', 'outdent', 'indent', '|', 'bold', 'italic', 'link', 'underline', 'strikethrough',
+    'code','subscript', 'superscript', 'highlight', '|', 'codeBlock', 'sourceEditing', 'insertImage',
+                'bulletedList', 'numberedList', 'todoList', '|',  'blockQuote', 'imageUpload', '|',
+                'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', 'mediaEmbed', 'removeFormat',
+                'insertTable',],
+    'image': {
+        'toolbar': ['imageTextAlternative', '|', 'imageStyle:alignLeft',
+                    'imageStyle:alignRight', 'imageStyle:alignCenter', 'imageStyle:side',  '|'],
+        'styles': [
+            'full',
+            'side',
+            'alignLeft',
+            'alignRight',
+            'alignCenter',
+        ]
+
+    },
+    'table': {
+        'contentToolbar': [ 'tableColumn', 'tableRow', 'mergeTableCells',
+        'tableProperties', 'tableCellProperties' ],
+        'tableProperties': {
+            'borderColors': customColorPalette,
+            'backgroundColors': customColorPalette
+        },
+        'tableCellProperties': {
+            'borderColors': customColorPalette,
+            'backgroundColors': customColorPalette
+        }
+    },
+    'heading' : {
+        'options': [
+            { 'model': 'paragraph', 'title': 'Paragraph', 'class': 'ck-heading_paragraph' },
+            { 'model': 'heading1', 'view': 'h1', 'title': 'Heading 1', 'class': 'ck-heading_heading1' },
+            { 'model': 'heading2', 'view': 'h2', 'title': 'Heading 2', 'class': 'ck-heading_heading2' },
+            { 'model': 'heading3', 'view': 'h3', 'title': 'Heading 3', 'class': 'ck-heading_heading3' }
+        ]
+    }
+},
+'list': {
+    'properties': {
+        'styles': 'true',
+        'startIndex': 'true',
+        'reversed': 'true',
+    }
+}
+}
+#-------------------CKEDITOR-------------------
+
+#-------------------CORREO-------------------
 DEFAULT_FROM_EMAIL = 'clothinfscoremain@outlook.es'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.outlook.com'
+EMAIL_HOST = 'smtp.outlook.com'  
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = DEFAULT_FROM_EMAIL
+EMAIL_HOST_USER = DEFAULT_FROM_EMAIL 
 EMAIL_HOST_PASSWORD = 'ClothInfs123'
+#-------------------CORREO-------------------
+#clothinfscoremain@outlook.es
+#ClothInfs123
